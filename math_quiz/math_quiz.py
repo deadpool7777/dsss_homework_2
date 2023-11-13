a169287fca4213da470fe82e4370af4e0405bd7a
deadpool7777 <alwinsolly@gmail.com>
2023-11-13 23:12:51 +0100

    modified setup file

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -16,8 +16,8 @@ def calculate(num1, num2, operator):
     problem = f"{num1} {operator} {num2}"
     if operator == '+':
         answer = num1 + num2
-    elif operator == '+':
-        answer = num1 + num2
+    elif operator == '-':
+        answer = num1 - num2
     else:
         answer = num1 * num2
     return problem, answer
--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your_project',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'your_script = your_module.module_file:main',
+        ],
+    },
+)
 
